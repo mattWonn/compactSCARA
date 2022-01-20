6b8c364db28730d1ebaa1de705d898c2bab69015
Rinz <Rinz@LAPTOP-LH7P1DT4.wifi.bcit.ca>
2022-01-20 15:06:35 -0800

    hello

---
+++ testing_git/main.c
@@ -0,0 +1,18 @@
+#include <msp430.h> 
+
+
+/**
+ * main.c
+ */
+int main(void)
+{
+	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+	
+	return 0;
+
+
+	int newitem =43;
+    // you yo yo
+	// testing 123
+
+}
