9a9b3c4d65401deed6ebc2d5ca8b8adc6cb2226d
mattWonn <98129680+mattWonn@users.noreply.github.com>
2022-02-14 17:46:47 -0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <msp430.h> 
+
+
+/**
+ * main.c
+ */
+int main(void)
+{
+	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+	
+	return 0;
+
+	dsfasdf123
+	// hello 123
+	// NASCAR
+	int thislove = 56;
+	int newitem =43;
+    // you yo yo
+	// anyone home?
+
+
+}
