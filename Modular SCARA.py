24465bbf7db64e1071fa7ad6d669598765a4dfa9
mattWonn <98129680+mattWonn@users.noreply.github.com>
2022-04-29 13:29:06 -0700

    update

--- Modular SCARA.py
+++ Modular SCARA.py
@@ -1,99 +1,115 @@
 from cgitb import enable
+from os import popen
 from tkinter import *
 import time
 import math
+import tkinter.ttk as ttk
+from SCARA import SCARA
 from zlib import DEFLATED
-#import serial
+import serial.tools.list_ports
 
-def disableButtons():
-    buttonMJ["state"] = DISABLED
-    buttonMJC["state"] = DISABLED
-    buttonML["state"] = DISABLED
-    buttonMC["state"] = DISABLED
-    buttonDisp["state"] = DISABLED
-    buttonReset["state"] = DISABLED
+portList=serial.tools.list_ports.comports()
+portLength = len(portList)
+baud = 115200
 
-def enableButtons():
-    buttonMJ["state"] = "normal"
-    buttonMJC["state"] = "normal"
-    buttonML["state"] = "normal"
-    buttonMC["state"] = "normal"
-    buttonDisp["state"] = "normal"
-    buttonReset["state"] = "normal"
 
+#----------------------- GUI commands -------------------------------
 def moveJ():
-    disableButtons()
-    global indexCMD
-    indexCMD = 2
-    print(eJ1.get())
-    print(eJ2.get())
-    print(armSolution.get())
-    print(toolPosition.get())
-    time.sleep(1)
+    screen.grab_release()
+    j1anglePulses = eJ1.get()*SCARA.pulses_per_degrees
+    j2anglePulses = eJ1.get()*SCARA.pulses_per_degrees
+    # robot.moveJ(j1anglePulses, j2AnglePulses, toolPosition.get())
+    screen.grab_set()
     
-    enableButtons()
-    
-
 def moveJcoord():
-    disableButtons()
-    global indexCMD
-    indexCMD = 3
-    print(eX.get())
-    print(eY.get())
-    print(armSolution.get())
-    print(toolPosition.get())
-    time.sleep(1)
-    enableButtons()
+    screen.grab_release()
+    #robot.moveJcoord(eX.get(), eY.get(), armSolution.get(), toolPosition.get())
+    screen.grab_set()
 
 def moveL():
-    disableButtons()
-    global indexCMD
-    indexCMD = 4
-    x = eX.get()
-    y = eY.get()
-    print(x)
-    print(y)
-    print(armSolution.get())
-    print(toolPosition.get())
-    time.sleep(1)
-    enableButtons()
+    screen.grab_release()
+    #robot.moveL(eX.get(), eY.get(), armSolution.get(), toolPosition.get())
+    screen.grab_set()
 
 def moveC():
-    disableButtons()
-    global indexCMD
-    indexCMD = 5
-    startAng = eStartAng.get()
-    endAng = eEndAng.get()
-    radius = eArc.get()
-    print(startAng)
-    print(endAng)
-    print(radius)
-    print(armSolution.get())
-    print(toolPosition.get())
-    time.sleep(1)
-    enableButtons()
+    screen.grab_release()
+    #robot.moveC(eStartAng.get(), eEndAng.get(), eArc.get(), armSolution.get(), toolPosition.get())
+    screen.grab_set()
 
 def resetPosition():
-    disableButtons()
-    global indexCMD
-    indexCMD = 6
-    #send reset position command to msp430
-    time.sleep(1)
-    enableButtons()
+    screen.grab_release()
+    #robot.zeroEncoders()
+    screen.grab_set()
 
 def stopProgram():
-    global indexCMD
-    indexCMD = 1
+    pass
+    #robot.doStop =1
 
 def quitProgram():
-    global indexCMD
-    indexCMD = 0
+    #robot.doStop=1
+    pass
     quit()
+#-------------------------------------------------------------------------------
+
+
+#----------------------User created Move J -------------------------------------
+def moveJProgram():
+    screen.grab_release() # disables the GUI command screen
+    global indexCMD
+    indexCMD = 8
+    # code to send both joint angles to the msp430
+    screen.grab_set() # enables the GUI command screen
+
+
+#--------------------- User created Move L ---------------------------------------
+def moveLProgram():
+    screen.grab_release()
+    global A_MAX_LINEAR
+    #Xa, user provided
+    Xa = 0
+    #Xb, user provided
+    #Ya, user provided
+    Ya = 0
+    #Yb, user provided
+    deltaX =0
+    deltaY =0
+    deltaD = 0
+    tInc = 0
+
+    deltaD = math.sqrt(pow(deltaX, 2) + pow(deltaY, 2))
+    timeForMove = math.sqrt((abs(deltaD)*2*PI)/A_MAX_LINEAR)
+    w = (2*(PI))/timeForMove
+    arrayLength = (timeForMove/T_UPDATE)+1
+
+    for tInc in range(arrayLength):
+        # now you must determine all of the x,y coordinates for every
+        # index of the arrayLength throughout the line based on the time of the move.
+
+        #The displacement along the line from 0 to D with respect to time is d(t).
+        d = ((A_MAX_LINEAR * (tInc*T_UPDATE))/w -  (A_MAX_LINEAR)*(math.sin(w*(tInc*T_UPDATE)))/pow(w,2))
+
+        #from the displacement along the line, and the known start and end coordinates of the line,
+        # use this formula to compute the corrisponding (x, y) coordinate of the move
+        x = Xa + (d*deltaX)/deltaD 
+        y = Ya + (d*deltaY)/deltaD
+        
+        # to do:
+        # check that the x, y coordinates are ouside of the robot's base boundary.
+        # compute the joint angles using inverse kinematics.
+        # convert the joint angles from degrees to pulses of the motor using the constant 9.48866 Pulses of the motor per degree of rotation
+
+
+    for tInc in range(arrayLength):
+        # in send over two array values in x,y coordinates if the array was computed successfully
+        indexCMD = 9 
+    
+    screen.grab_set()
+
 
 
 screen = Tk()
 screen.title("Modular SCARA")
-screen.geometry('800x800')
+screen.geometry('900x800')
 #screen.config(bg='#DCDCDC')
 
 blank = Label(screen, text="  ")
@@ -102,7 +118,32 @@ blank.grid(row=0, column=0, pady=5)
 armSolution = IntVar()
 toolPosition = IntVar()
 indexCMD = IntVar()
+displayMainPage=0
 
+portOption = StringVar(screen)
+portOption.set("  Select  ") # default value
+
+def selectPort(portOption):
+    pop.grab_release()
+    pop.destroy()
+    robot = SCARA(portOption, robot.defaultBAUD)
+
+
+
+global pop
+pop = Toplevel(screen)
+pop.title("My Popup")
+pop.geometry("250x150")
+pop.grab_set()
+
+pop_Label = Label(pop, text="Select a COMM port")
+pop_Label.pack()
+
+my_frame = Frame(pop)
+my_frame.pack()
+
+portMenu = OptionMenu(pop, portOption, *portList, command=selectPort)
+portMenu.pack()
 
 #------------------- Label section---------------------
 theta1Label = Label(screen, text="Joint 1 angle:", font=('Calibri 14'))
@@ -145,6 +186,8 @@ eJ1.focus
 eJ2 = Entry(screen, width=15, borderwidth=5)
 eJ2.grid(row=3, column=1)
 eJ2.insert(0, "0")
+
+
 
 # arm coordiante input fields
 eX = Entry(screen, width=15, borderwidth=5)
@@ -196,55 +239,6 @@ button.grid(row=15, column=3)
 A_MAX_LINEAR = 10
 T_UPDATE = 0.01
 PI = 3.141593
-#---------------------- Move J -------------------------------------
-def moveJProgram():
-    disableButtons()
-    global indexCMD
-    indexCMD = 8
-    # code to send both joint angles to the msp430
-    enableButtons()
-
-def moveLProgram():
-    global A_MAX_LINEAR
-    #Xa, user provided
-    Xa = 0
-    #Xb, user provided
-    #Ya, user provided
-    Ya = 0
-    #Yb, user provided
-    deltaX =0
-    deltaY =0
-    deltaD = 0
-    tInc = 0
-
-    deltaD = math.sqrt(pow(deltaX, 2) + pow(deltaY, 2))
-    timeForMove = math.sqrt((abs(deltaD)*2*PI)/A_MAX_LINEAR)
-    w = (2*(PI))/timeForMove
-    arrayLength = (timeForMove/T_UPDATE)+1
-
-    for tInc in range(arrayLength):
-        # now you must determine all of the x,y coordinates for every
-        # index of the arrayLength throughout the line based on the time of the move.
-
-        #The displacement along the line from 0 to D with respect to time is d(t).
-        d = ((A_MAX_LINEAR * (tInc*T_UPDATE))/w -  (A_MAX_LINEAR)*(math.sin(w*(tInc*T_UPDATE)))/pow(w,2))
-
-        #from the displacement along the line, and the known start and end coordinates of the line,
-        # use this formula to compute the corrisponding (x, y) coordinate of the move
-        x = Xa + (d*deltaX)/deltaD 
-        y = Ya + (d*deltaY)/deltaD
-        
-        # to do:
-        # check that the x, y coordinates are ouside of the robot's base boundary.
-        # compute the joint angles using inverse kinematics.
-        # convert the joint angles from degrees to pulses of the motor using the constant 9.48866 Pulses of the motor per degree of rotation
-
-
-    for tInc in range(arrayLength):
-        # in send over two array values in x,y coordinates if the array was computed successfully
-        indexCMD = 9 
-
-
 
 
 
